7d9c29736e0ece9481a7ee042073dfad3281f815
Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>
2022-02-16 22:46:38 -0300

    feat: add button component
    add button component and add roboto font.

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  padding: 14px 48px;
+  width: 146px;
+  height: 47px;
+  background-color: #1E86FF;
+  font-family: 'Roboto', sans-serif;
+  color: #FFF;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  border: none;
+  border-radius: 4px;
+  box-sizing: border-box;
+`;
+
+export default Button;
