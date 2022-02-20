36d064f73fa9a07a8895a26362fe5c01233ba72d
Levy Mateus Macedo <levy.macedo.ext@dasa.com.br>
2022-02-20 11:27:56 -0300

    feat: add use pagination hook

---
+++ src/hooks/usePagination.ts
@@ -0,0 +1,49 @@
+import { useState, useCallback } from 'react';
+
+type PaginationProps = {
+  pageCount: number
+  initialState?: number
+}
+
+type PaginationFuncs = {
+  set: (pageValue: number) => void
+  next: () => void
+  prev: () => void
+  count: number
+}
+
+type Pagination = [number, PaginationFuncs]
+
+function usePagination({ pageCount, initialState = 1 }: PaginationProps): Pagination {
+  const [page, setPage] = useState<number>(initialState);
+
+  const inc = (value: number) => {
+    if (value + 1 <= pageCount) {
+      return page + 1;
+    }
+    return pageCount;
+  };
+
+  const dec = (value: number) => {
+    if (value > 1) {
+      return value - 1;
+    }
+    return 1;
+  };
+
+  const next: PaginationFuncs['next'] = useCallback(() => setPage(inc), [page]);
+
+  const prev: PaginationFuncs['prev'] = useCallback(() => setPage(dec), [page]);
+
+  const set: PaginationFuncs['set'] = useCallback((value) => {
+    if (value <= pageCount && value >= 1) {
+      setPage(value);
+    }
+  }, []);
+
+  return [page, {
+    set, next, prev, count: pageCount,
+  }];
+}
+
+export default usePagination;
